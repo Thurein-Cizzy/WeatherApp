3b839fb958dfc954940a7ea534efcddf78a39c92
Cizzy <thureinzawmin31@gmail.com>
2025-04-29 23:23:29 +0630

    responsive nav-bar added

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("hello , thurein");
+let userLatitude;
+let userLongitude;
+let p;
+
+const getUserPosition = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (p) => {
+        resolve(p.coords);
+      },
+      (e) => {
+        reject(e);
+      }
+    );
+  });
+};
+
+async function getData(lati, long) {
+  let url = `https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${long}&hourly=temperature_2m&models=metno_seamless&current=temperature_2m,is_day`;
+  console.log(url);
+  let response = await fetch(url);
+
+  let result = await response.json();
+  console.log(result);
+}
+
+async function getWeather() {
+  try {
+    const coords = await getUserPosition();
+    userLatitude = coords.latitude;
+    userLongitude = coords.longitude;
+    getData(userLatitude, userLongitude);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getWeather();
