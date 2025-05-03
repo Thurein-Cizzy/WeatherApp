3e63272505561db0a52308fe31df0ae2a6fb9b22
Cizzy <thureinzawmin31@gmail.com>
2025-05-03 23:27:14 +0630

    Base Ui desing for current time data done

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const getUserPosition = () => {
 };
 
 async function getData(lati, long) {
-  let url = `https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${long}&hourly=temperature_2m&models=metno_seamless&current=temperature_2m,is_day`;
+  let url = `https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${long}&hourly=temperature_2m,cloud_cover,rain,precipitation,precipitation_probability,wind_speed_10m&current=temperature_2m,is_day,rain,showers,snowfall,cloud_cover,precipitation,relative_humidity_2m,apparent_temperature,wind_speed_10m,wind_direction_10m,wind_gusts_10m,weather_code`;
   console.log(url);
   let response = await fetch(url);
 
@@ -29,7 +29,7 @@ async function getWeather() {
     const coords = await getUserPosition();
     userLatitude = coords.latitude;
     userLongitude = coords.longitude;
-    getData(userLatitude, userLongitude);
+    getData(21.989322, 96.068303);
   } catch (e) {
     console.log(e);
   }
